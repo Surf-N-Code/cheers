dde329994219694444f901ab044009ebebc2d6ed
Norman <ndilthey@gmail.com>
2019-01-26 20:09:58 +0100

    WA fix

--- src/Controller/ParseController.php
+++ src/Controller/ParseController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\ParseQueue;
 use App\Repository\ParseQueueRepository;
+use App\Utils\DanielParser;
 use App\Utils\EbayParser;
 use App\Utils\Parser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +17,16 @@ class ParseController extends AbstractController
      * @Route("/getProductInfo", name="parse")
      */
     public function amazonParse(Parser $parser)
+    {
+        $status = [];
+        array_push($status, $parser->getProductInfo());
+        return new JsonResponse($status);
+    }
+
+    /**
+     * @Route("/daniel", name="parse")
+     */
+    public function danielParse(DanielParser $parser)
     {
         $status = [];
         array_push($status, $parser->getProductInfo());
