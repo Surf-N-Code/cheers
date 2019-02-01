adf8d48300daecd947c9cdd596aee3ad0a6f65dc
Norman <ndilthey@gmail.com>
2019-02-01 22:15:12 +0100

    parsecontroller without daniel

--- src/Controller/ParseController.php
+++ src/Controller/ParseController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\ParseQueue;
-use App\Repository\ParseQueueRepository;
 use App\Utils\DanielParser;
-use App\Utils\EbayParser;
 use App\Utils\Parser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -22,14 +19,14 @@ class ParseController extends AbstractController
         array_push($status, $parser->getProductInfo());
         return new JsonResponse($status);
     }
-
-    /**
-     * @Route("/daniel", name="parse")
-     */
-    public function danielParse(DanielParser $parser)
-    {
-        $status = [];
-        array_push($status, $parser->getProductInfo());
-        return new JsonResponse($status);
-    }
+//
+//    /**
+//     * @Route("/daniel", name="parse")
+//     */
+//    public function danielParse(DanielParser $parser)
+//    {
+//        $status = [];
+//        array_push($status, $parser->getProductInfo());
+//        return new JsonResponse($status);
+//    }
 }
